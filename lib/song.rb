c7d4f82cbaa1e3289ce7cfdca00c82d21f64b62e
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-05-07 23:49:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,19 +3,19 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@all = []
- 
+
   def initialize(name)
     @name = name
     @@all << self
   end
- 
+
   def artist_name
     self.artist == nil ? nil : self.artist.name
    end
   end
- 
+
   def self.all
    @@all
   end
- 
+
 end
