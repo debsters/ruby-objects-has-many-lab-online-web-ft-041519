268c847df8a24fd73c746e6c68116817ecc6bda8
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-05-07 23:17:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,32 +1,35 @@
 class Artist
   attr_accessor :name
 
-  @@song_count = 0
+  @@all = []
 
   def initialize(name)
     @name = name
     @songs = []
+    @@all << self
   end
 
   def add_song(song)
     @songs << song
     song.artist = self
-    @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
-    @@song_count += 1
   end
 
   def songs
     @songs
   end
 
+  def self.all
+    @@all
+  end
+  
   def self.song_count
-    @@song_count
+    all.size
   end
 
 end
